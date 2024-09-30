e63eb6cc38244ed5da8a5ad7e4f4c6ca93d9011a
AliIdris1 <166216558+AliIdris1@users.noreply.github.com>
2024-09-30 23:40:01 +0300

    Update Hero.tsx

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
       </div>
       <div className='hero__image-container'>
         <div className='hero__image'>
-          <Image src="/hero.png"  alt='hero' className='object-contain' fill />
+        <img src="/hero.png" alt="hero" className="object-contain w-full h-full "/>
         </div>
           <div className='hero__image-overlay'></div>
       </div>
