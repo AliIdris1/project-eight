4811df131336762497708fce34d6ac834bc3c1fb
AliIdris1 <166216558+AliIdris1@users.noreply.github.com>
2024-10-05 14:38:56 +0300

    Update Hero.tsx

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
   const handleScroll = () => {}
   return (
     <div className='hero'>
-      <div className='pt-36 padding-x flex-1'>
+      <div className='hdiv1'>
         <h1 className='hero__title'>Find, book, or rent a car-quickly and easily!</h1>
         <p className='hero__subtitle'>Streamline your car rental experience with our effortless booking process</p>
         <CustomButton title="Explore Cars" containerstyles="hero-btn" handleclick={handleScroll}  />
